195c6784dd1881e090f848aec9bb719e93b34446
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-09-28 04:21:47 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import lang, os
 
 LN = os.environ['LN']
 TR = lang.languages
-Sudo_ID = 438131290
+Sudo_ID = 5063062493
 
 SimSimi = dict(url='http://api.simsimi.com/request.p', ft="2.3", lc='LN', text='Hi')
 SimSimi['key']  = os.environ['SECRET_KEY_SIMSIMI']
